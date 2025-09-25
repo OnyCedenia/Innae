e8831a3303c7ddaa854262278d6750e4c05b3d74
rose <rosefalco.pro@gmail.com>
2025-09-25 13:59:30 +0200

    fix: add img folder in asset

--- src/Controller/FrontOffice/HomeController.php
+++ src/Controller/FrontOffice/HomeController.php
@@ -2,20 +2,17 @@
 
 namespace App\Controller\FrontOffice;
 
-use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
-    {   //recuperer les postes
-        // $posts = $postRepository ->findAll();
-        //transmettre les postes a la vue
+    {   
         return $this->render('FrontOffice/home.html.twig', [
-            // 'posts' => $posts,
+           
         ]);
     }
 
