c1173ebd1c03cca37010760dab7f5a0c4fbc702d
rose <rosefalco.pro@gmail.com>
2025-09-25 14:25:23 +0200

    fix(style): move style in assets folder for asset-map compile

---
+++ assets/home.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const track = document.querySelector(".carousel-track");
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector(".carousel-btn.next");
+    const prevButton = document.querySelector(".carousel-btn.prev");
+
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        track.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    nextButton.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateCarousel();
+    });
+
+    prevButton.addEventListener("click", () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        updateCarousel();
+    });
+
+    
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateCarousel();
+    }, 4000);
+});
